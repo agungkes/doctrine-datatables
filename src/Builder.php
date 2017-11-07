9d5a4d256db430e743ef2c951232f6aabd39f932
Fabian Kreuzkam <fabian@kreuzkam.com>
2017-11-07 13:39:26 +0100

    fixed Annotation

--- src/Builder.php
+++ src/Builder.php
@@ -326,7 +326,7 @@ class Builder
     }
 
     /**
-     * @param array $requestParams
+     * @param string $delimiter
      * @return static
      */
     public function withSearchDelimiter($delimiter)
